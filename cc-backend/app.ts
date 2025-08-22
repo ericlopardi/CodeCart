f0e09b044546c19ab4662176087c58b4aee109b6
jshelley8117 <jshelley8117@gmail.com>
2025-08-22 08:23:38 -0500

    refactor: fix fmt

--- cc-backend/app.ts
+++ cc-backend/app.ts
@@ -5,8 +5,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on Port ${port}`);
