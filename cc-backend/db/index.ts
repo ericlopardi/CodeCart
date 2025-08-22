be07b918cb7640fdeee895d2a1b3d8d0433b3b2d
jshelley8117 <103763460+jshelley8117@users.noreply.github.com>
2025-08-22 09:19:43 -0500

    Create backend server (#3)
    * feat: introduce backend server
    
    * git practice
    
    * refactor: fix fmt
    
    * refactor: modify db init
    
    * refactor: remove file
    
    ---------
    
    Co-authored-by: Royogechi <116686908+woytiller@users.noreply.github.com>

--- cc-backend/db/index.ts
+++ cc-backend/db/index.ts
@@ -1,8 +1,16 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres';
+import dotenv from 'dotenv';
 
-config({ path: '.env' });
+dotenv.config();
 
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle({ client });
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+    throw new Error('DATABASE_URL is not defined');
+}
+console.log('Database URL:', connectionString);
+
+const client = postgres(connectionString, {
+    ssl: 'require'
+});
+export const db = drizzle(client);
