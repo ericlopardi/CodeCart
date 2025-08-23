6c26bc539ff00a840df07c5059c26f4f72b2d0b1
jshelley8117 <jshelley8117@gmail.com>
2025-08-23 14:48:56 -0500

    feat: introduce skeleton for register flow

---
+++ cc-backend/services/auth/registerService.ts
@@ -0,0 +1,17 @@
+import { Request } from 'express';
+
+export const initRegister = async (req: Request) => {
+
+}
+
+const createCustomerRecord = async (data: any) => {
+
+}
+
+const registerWithSupabase = async (data: any) => {
+
+}
+
+const createUserRecord = async (data: any) => {
+
+}
