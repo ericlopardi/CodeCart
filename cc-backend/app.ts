988112199510b76951c26fc67a44b33ddbdc27f1
jshelley8117 <103763460+jshelley8117@users.noreply.github.com>
2025-08-24 15:18:11 -0500

    COD-14: Implement Login & Auth Middleware (#4)
    * feat: introduce login functionality
    
    * refactor: remove node_modules
    
    * feat: introduce auth middleware and finalize login route
    
    * refactor: modify error response for validation failures
    
    * refactor: address MR feedback

--- cc-backend/app.ts
+++ cc-backend/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes/index'
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use('/api', router)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
